ad3fb5674e34533b995858e6a22529053b1c327d
aceto-balsamico <horihori4179@gmail.com>
2025-06-30 22:06:59 +0900

    add custom strcmp advanced

--- Create_DefaultC_Function.c
+++ Create_DefaultC_Function.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+#ifdef _WIN32
+#include <windows.h>
+double get_time_sec()
+{
+    static LARGE_INTEGER freq;
+    static int initialized = 0;
+    LARGE_INTEGER now;
+    if (!initialized) {
+        QueryPerformanceFrequency(&freq);
+        initialized = 1;
+    }
+    QueryPerformanceCounter(&now);
+    return (double)now.QuadPart / (double)freq.QuadPart;
+}
+#else
+#include <time.h>
+double get_time_sec()
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec + ts.tv_nsec / 1e9;
+}
+#endif
+
 
 int custom_strlen(char* str)
 {
@@ -50,6 +74,7 @@ int custom_strcmp(char* str1, char* str2)
     {
         return -1;
     }
+    
     int char_index = 0;
     while(str1[char_index] != '\0' && str2[char_index] != '\0')
     {
@@ -64,6 +89,23 @@ int custom_strcmp(char* str1, char* str2)
         return 1;
     }
     return 0; 
+}
+
+int custom_strcmp_advanced(char* array, char* reference)
+{
+    if(array == NULL || reference == NULL)
+    {
+        return -1;
+    }
+
+    if((*(long long*)array ^ *(long long*)reference) == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
 }
 
 int custom_strstr(char* origin_str, char* search_str)
@@ -117,9 +159,39 @@ int  main()
         printf("'World' not found in the string.\n");
     }
 
+    double current_time = get_time_sec();
+    int check_equal = 0;
+    for(int i = 0; i < 1000000; i++)
+    {
+        custom_strcmp(str1, str2);
+    }
+    // int check_equal = custom_strcmp_advanced("hello", "NULL");
+    double elapsed_time = get_time_sec() - current_time;
+    printf("Time taken for custom_strcmp_advanced: %f seconds\n", elapsed_time);
+    if (check_equal == 0)
+    {
+        printf("Strings are equal.\n");
+    }
+    else
+    {
+        printf("Strings are not equal.\n");
+    }
+    
+
+
+    current_time = get_time_sec();
     char* string1 = "Hello1";
     char* string2 = "Hello";
-    int result = custom_strcmp(string1, string2);
+    int result = 0;
+    for(int i = 0; i < 1000000; i++)
+    {
+        custom_strcmp(string1, string2);
+    }
+    // int result = custom_strcmp(string1, string2);
+    elapsed_time = get_time_sec() - current_time;
+    printf("Time taken for custom_strcmp: %f seconds\n", elapsed_time);
+
+
     if (result == 0) 
     {
         return 0;
